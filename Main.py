dfbae9bc98ffa2d69df9e7bee9aee2644206bac8
salvatore esposito <salvatoreesposito@airdisalvatore.home>
2020-08-04 19:52:06 +0100

    Modified main, fixed indentation

--- Main.py
+++ Main.py
@@ -28,25 +28,24 @@ def create_roi_for_list(scan, idx, rois, scanidx):
     return rois
 
 
-def peakonly(num_of_scans=False):
+def peakonly(num_of_scans=False, filepath=None):
     rois = []
     dead_rois = []
     intensity_threshold = 10000
-    mzthreshold_min = 445
-    mzthreshold_max = 450
+    mzthreshold_min = 137.5
+    mzthreshold_max = 138.5
     delta_mz = 0.005
-    rt_min = 430
-    rt_max = 480
-    run = pymzml.run.Reader(
-        "/Users/salvatoreesposito/Downloads/Beer_multibeers_1_fullscan1.mzML")
+    rt_min = 560
+    rt_max = 600
+    if filepath is None:
+        filepath = "/Users/salvatoreesposito/Downloads/Beer_multibeers_1_fullscan1.mzML"
+    run = pymzml.run.Reader(filepath)
     start = time.time()
-    print("Hello")
     for scanidx, scan in enumerate(run):
         if num_of_scans != False and scanidx == num_of_scans:
             break
-        print(scanidx)
         extended_rois = []
-        if scan.scan_time[0] <= rt_max and scan.scan_time[0] >= rt_min:
+        if scan.scan_time[0]*60. <= rt_max and scan.scan_time[0]*60. >= rt_min:
             # mz can go into any ROI, but not mulitple roi
             # loop over the mz values of a scan as well as the indexes
             for idx, mz in enumerate(scan.mz):
@@ -78,7 +77,8 @@ def peakonly(num_of_scans=False):
                                     closest_roi_index = high_index
                         # use the index to find the closest roi in the rois list
                         # and get the mean mz from it
-                        closest_dist = abs(rois[closest_roi_index].mean_mz - mz)
+                        closest_dist = abs(
+                            rois[closest_roi_index].mean_mz - mz)
                         # check if the closest distance is less than the threshold
                         if closest_dist < delta_mz:
                             # and use the index to find the roi in the rois list
@@ -92,9 +92,12 @@ def peakonly(num_of_scans=False):
                         else:
                             extended_rois = create_roi_for_list(
                                 scan, idx, extended_rois, scanidx)
-                    dead_rois = dead_rois + rois
-                    rois = extended_rois
+        dead_rois = dead_rois + rois
+        rois = extended_rois
+        print(len(rois), len(dead_rois), len(extended_rois))
+
     dead_rois = dead_rois + extended_rois
+    print(len(dead_rois))
     # print(extended_rois)
 
 # Check and cleanup
@@ -102,7 +105,7 @@ def peakonly(num_of_scans=False):
     for saved_roi in dead_rois:
         peaklist = saved_roi.peak_list
         pmin = np.array([peak.mz for peak in peaklist]).min()
-        # some rois have only 1 intesity 
+        # some rois have only 1 intesity
         if len(peaklist) > 1:
             # if pmin < delta_mz:
             bisect.insort(completed_rois, saved_roi)
@@ -110,28 +113,28 @@ def peakonly(num_of_scans=False):
     end = time.time()
     print(end - start)
     print(len(completed_rois))
-
     # for roi in completed_rois:
     #     print(roi)
     #     # for peak in roi.peak_list:
     #     #     print(peak)
     # print(len(completed_rois))
 
-    classifier = CNN.load_model()
-    for roi in completed_rois:
-        roi = nn_roi.ROI(scan=[roi.peak_list[0], roi.peak_list[-1]],
-                         rt=[roi.peak_list[0].rt,
-                             roi.peak_list[-1].rt],
-                         i=[peak.i for peak in roi.peak_list],
-                         mz=[peak.mz for peak in roi.peak_list],
-                         mzmean=roi.mean_mz)
-        if len(roi.i) != 1:
-            print("ROI intensity: " + str(roi.mzmean), "CNN classifier: " +
-                  str(CNN.classifier_prediction(roi, classifier, torch.device("cpu"), points=256)))
-        else:
-            print("ROI intensity: " + str(roi.mzmean), "CNN classifier: " +
-                  str(CNN.classifier_prediction(roi, classifier, torch.device("cpu"), points=256)))
+    # classifier = CNN.load_model()
+    # for roi in completed_rois:
+    #     roi = nn_roi.ROI(scan=[roi.peak_list[0], roi.peak_list[-1]],
+    #                      rt=[roi.peak_list[0].rt,
+    #                          roi.peak_list[-1].rt],
+    #                      i=[peak.i for peak in roi.peak_list],
+    #                      mz=[peak.mz for peak in roi.peak_list],
+    #                      mzmean=roi.mean_mz)
+    #     if len(roi.i) != 1:
+    #         print("ROI intensity: " + str(roi.mzmean), "CNN classifier: " +
+    #               str(CNN.classifier_prediction(roi, classifier, torch.device("cpu"), points=256)))
+    #     else:
+    #         print("ROI intensity: " + str(roi.mzmean), "CNN classifier: " +
+    #               str(CNN.classifier_prediction(roi, classifier, torch.device("cpu"), points=256)))
 
-
-# if __name__ == "__main__":
-peakonly(400)
+    return completed_rois
+peakonly()
+# # if __name__ == "__main__":
+# peakonly(400)
